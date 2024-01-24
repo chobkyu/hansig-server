8deee81b49783794e359a47c18af57db98249df5
chobkyu <98572357+chobkyu@users.noreply.github.com>
2024-01-24 10:51:51 +0900

    update

--- src/router/owner/index.ts
+++ src/router/owner/index.ts
@@ -3,4 +3,13 @@ const ctrl = require('./owner.ctrl');
 
 const router =express.Router();
 
+router.get('/menu/:id',ctrl.output.getMenuList);
+
+router.post('/',ctrl.process.ownerSignUp);
+router.post('/login',ctrl.process.ownerSignIn);
+router.post('/menu',ctrl.process.ownerMenu);
+
+router.delete('/menu',ctrl.process.deleteMenu);
+router.patch('/menu',ctrl.process.updateMenu);
+
 module.exports = router;
--- src/router/owner/owner.ctrl.ts
+++ src/router/owner/owner.ctrl.ts
@@ -1,11 +1,31 @@
 import express,{Express,Request,Response} from 'express';
 
 const output = {
-
+    getMenuList :async (req:Request,res:Response) => {
+        
+    }
 }
 
 const process = {
+    ownerSignUp :async (req:Request,res:Response) => {
+        //사업자로 회원 가입
+    },
 
+    ownerSignIn : async (req:Request,res:Response) => {
+
+    },
+
+    ownerMenu :async (req:Request, res:Response) => {
+        
+    },
+
+    deleteMenu :async (req:Request,res:Response) => {
+        
+    },
+
+    updateMenu :async (req:Request, res:Response) => {
+        
+    }
 }
 
 module.exports = {
