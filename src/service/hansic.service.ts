a67cf76d909e5c93650a149c0221bcca46361880
chobkyu <98572357+chobkyu@users.noreply.github.com>
2024-01-31 14:16:51 +0900

    update

--- src/service/hansic.service.ts
+++ src/service/hansic.service.ts
@@ -1,4 +1,4 @@
-import {hansics, PrismaClient} from "@prisma/client";
+import { hansics, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 /*
@@ -16,7 +16,7 @@ class HansicService {
   async getHansicDate() {
     try {
       const data =
-          await prisma.hansics.findMany({include : {location : true}});
+        await prisma.hansics.findMany({ include: { location: true } });
       return { data, success: true }
     } catch (err) {
       console.error(err);
@@ -26,7 +26,24 @@ class HansicService {
   //식당id로 조회
   async get(restaurantId: number): Promise<any> {
     try {
-      const data = await prisma.$queryRaw<any>`SELECT hs.id,hs.name,hs.addr,hs."userStar",hs.google_star,hs.location_id,ls.location,si."imgUrl" FROM hansics as hs INNER JOIN location as ls on hs.location_id=ls.id LEFT JOIN "sicdangImg" as si on hs.id=si."hansicsId" WHERE hs.id=${restaurantId}`;
+      const data = await prisma.$queryRaw<any>`
+        SELECT 
+          hs.id,
+          hs.name,
+          hs.addr,
+          hs."userStar",
+          hs.google_star,
+          hs.location_id,
+          ls.location,
+          si."imgUrl" 
+        FROM hansics as hs 
+        INNER JOIN location as ls 
+        on hs.location_id=ls.id 
+        LEFT JOIN "sicdangImg" as si 
+        on hs.id=si."hansicsId" 
+        WHERE hs.id=${restaurantId}
+      `;
+
       console.log(data);
       if (data[0]) {
         return data[0];
@@ -38,21 +55,39 @@ class HansicService {
     }
   }
   //지역id로 조회
-  async getFromLocation(locationId: number): Promise<any[]|false> {
+  async getFromLocation(locationId: number): Promise<any[] | false> {
     try {
-      const data = await prisma.$queryRaw<any[]>`SELECT hs.id,hs.name,hs.addr,hs."userStar",hs.google_star,hs.location_id,ls.location,si."imgUrl" FROM hansics as hs INNER JOIN location as ls on hs.location_id=ls.id LEFT JOIN "sicdangImg" as si on hs.id=si."hansicsId" WHERE hs.location_id=${locationId} ORDER BY hs.id ASC`;
-        console.log(data);
-    if (data) {
-      return data;
-    } else {
-      return false;
-    }
+      const data = await prisma.$queryRaw<any[]>`
+        SELECT
+          hs.id,
+          hs.name,
+          hs.addr,
+          hs."userStar",
+          hs.google_star,
+          hs.location_id,
+          ls.location,
+          si."imgUrl"
+        FROM hansics as hs 
+        INNER JOIN location as ls 
+        on hs.location_id=ls.id 
+        LEFT JOIN "sicdangImg" as si 
+        on hs.id=si."hansicsId" W
+        HERE hs.location_id=${locationId} 
+        ORDER BY hs.id ASC
+      `;
+      
+      console.log(data);
+      if (data) {
+        return data;
+      } else {
+        return false;
+      }
     } catch (err) {
       return false
     }
   }
   //전체조회
-  async getAll(): Promise<any[]|false> {
+  async getAll(): Promise<any[] | false> {
     try {
       const data = await prisma.$queryRaw<any[]>`SELECT hs.id,hs.name,hs.addr,hs."userStar",hs.google_star,hs.location_id,ls.location,si."imgUrl" FROM hansics as hs INNER JOIN location as ls on hs.location_id=ls.id LEFT JOIN "sicdangImg" as si on hs.id=si."hansicsId" ORDER BY hs.id ASC`;
       console.log(data);
