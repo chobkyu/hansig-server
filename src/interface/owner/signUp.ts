dfc8d88eae026ca790df00edaf2ea87336c124fc
cxzaqq <roy1228@naver.com>
2024-02-08 20:13:17 +0900

    공공 데이터 api 추가

--- src/interface/owner/signUp.ts
+++ src/interface/owner/signUp.ts
@@ -1,10 +1,11 @@
 export interface SignUpData {
-    userId: string,
-    userPw: string,
-    userName: string,
-    userNickName : string,
-    ownerNum:string,
-    hansicdangName : string,
-    hansicdangAddr:string,
-    location_id:number
+  userId: string;
+  userPw: string;
+  userName: string;
+  userNickName: string;
+  ownerNum: string;
+  hansicdangName: string;
+  hansicdangAddr: string;
+  location_id: number;
+  startDate: string;
 }
