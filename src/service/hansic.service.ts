a4c2e2f61a2b5f04370962221049890182328acd
slugjeon <youngman0515@naver.com>
2024-02-15 14:14:59 +0900

    fix:hansic_get 수정

--- src/service/hansic.service.ts
+++ src/service/hansic.service.ts
@@ -18,18 +18,17 @@ const logger = require('../util/winston');
 export class HansicService {
   async getHansicDate() {
     try {
-      const data =
-        await prisma.hansics.findMany({ include: { location: true } });
+      const data = await prisma.hansics.findMany({include : {location : true}});
       return { data, success: true }
     } catch (err) {
       logger.error(err);
       return { success: false }
     }
   }
-  //좌표로 조회
+  // 좌표로 조회
   async getByPlace(lat: Number, lng: Number, userId?: Number) {
     try {
-      //좌표로 단일 검색
+      // 좌표로 단일 검색
       const data = await prisma.$queryRaw<any[]>`
         SELECT 
           hs.id,
@@ -60,29 +59,24 @@ export class HansicService {
       `;
 
       console.log(data);
-      //user정보가 넘어왔을시 favorite확인
+      // user정보가 넘어왔을시 favorite확인
 
       let favorite;
 
       if (userId) {
-        let findStar = await prisma.favorites.findFirst({ 
-          where: { 
-            userId: Number(userId), 
-            hansicsId: data[0].id 
-          }
-        });
+        let findStar = await prisma.favorites.findFirst(
+            {where : {userId : Number(userId), hansicsId : data[0].id}});
 
         favorite = findStar?.useFlag == null ? false : findStar.useFlag;
-      }
-      else {
-        //유저정보가 넘어오지 않은경우
+      } else {
+        // 유저정보가 넘어오지 않은경우
         favorite = false;
       }
 
       console.log(favorite);
       if (data.length > 0) {
         data[0].count = Number(data[0].count);
-        //즐겨찾기 되어 있으면 true
+        // 즐겨찾기 되어 있으면 true
         data[0].favorite = favorite;
         return data[0];
       } else {
@@ -94,8 +88,8 @@ export class HansicService {
       return false
     }
   }
-  //식당id로 단일 조회
-  async get(restaurantId: number, userId?: number): Promise<Object | false> {
+  // 식당id로 단일 조회
+  async get(restaurantId: number, userId?: number): Promise<Object|false> {
     try {
       let favorite;
       const data = await prisma.$queryRaw<any[]>`
@@ -126,18 +120,18 @@ export class HansicService {
         on hs.id=rd."hansicsId"
         WHERE hs.id=${restaurantId}
       `;
-      //user정보가 넘어왔을시 favorite확인
+      // user정보가 넘어왔을시 favorite확인
       if (userId) {
-        favorite = await prisma.favorites.findFirst({ where: { userId: userId, hansicsId: restaurantId } });
-
-      }
-      else {
-        //유저정보가 넘어오지 않은경우
+        let findStar = await prisma.favorites.findFirst(
+          {where : {userId : Number(userId), hansicsId : data[0].id}});
+      favorite = findStar?.useFlag == null ? false : findStar.useFlag;
+      } else {
+        // 유저정보가 넘어오지 않은경우
         favorite = false;
       }
       if (data.length > 0) {
         data[0].count = Number(data[0].count);
-        //즐겨찾기 되어 있으면 true
+        // 즐겨찾기 되어 있으면 true
         data[0].favorite = favorite ? true : false;
         return data[0];
       } else {
@@ -149,8 +143,8 @@ export class HansicService {
       return false;
     }
   }
-  //지역id로 지역내 식당리스트 조회
-  async getFromLocation(locationId: number): Promise<any[] | false> {
+  // 지역id로 지역내 식당리스트 조회
+  async getFromLocation(locationId: number): Promise<any[]|false> {
     try {
       const data = await prisma.$queryRaw<any[]>`
         SELECT
@@ -183,8 +177,8 @@ export class HansicService {
       return false
     }
   }
-  //전체조회
-  async getAll(): Promise<any[] | false> {
+  // 전체조회
+  async getAll(): Promise<any[]|false> {
     try {
       const data = await prisma.$queryRaw<any[]>`
         SELECT
@@ -219,39 +213,36 @@ export class HansicService {
     }
   }
 
-  //주소 -> 좌표 변환
+  // 주소 -> 좌표 변환
   async convert() {
     try {
       const response: any = await this.getAll();
       logger.info(response);
       for (var i = 0; i < response.length; i++) {
-        //logger.info(response[i].addr);
+        // logger.info(response[i].addr);
 
         if (response[i].addr != '주소 없음') {
           await this.tryGeo(response[i])
-
         }
       }
-      return { success: true };
+      return {success : true};
     } catch (err) {
       logger.error(err);
-      return { success: false };
+      return {success : false};
     }
   }
 
-  //kakao api 호출, 주소 -> 좌표 변환
+  // kakao api 호출, 주소 -> 좌표 변환
   async tryGeo(hansic: any) {
     try {
       const addr = hansic.addr
       const option = {
-        uri: 'https://dapi.kakao.com/v2/local/search/address',
-        qs: {
-          query: addr
-        },
-        headers: { Authorization: `KakaoAK ${process.env.kakao_api}` }
+        uri : 'https://dapi.kakao.com/v2/local/search/address',
+        qs : {query : addr},
+        headers : {Authorization : `KakaoAK ${process.env.kakao_api}`}
       }
 
-      //카카오 api 호출
+      // 카카오 api 호출
       request(option, async (err: any, response: any, body: any) => {
         logger.info(body);
         const obj = JSON.parse(body);
@@ -259,8 +250,8 @@ export class HansicService {
         if (!obj["documents"]?.length) {
           logger.info('can not find address');
         } else {
-          logger.info(obj["documents"][0].x)  //lng
-          logger.info(obj["documents"][0].y)  //lat 
+          logger.info(obj["documents"][0].x) // lng
+          logger.info(obj["documents"][0].y) // lat
           let lng = parseFloat(obj["documents"][0].x);
           let lat = parseFloat(obj["documents"][0].y);
 
@@ -275,25 +266,25 @@ export class HansicService {
     }
   }
 
-  //좌표 데이터베이스에 저장
+  // 좌표 데이터베이스에 저장
   async updateGeo(id: number, lng: number, lat: number) {
     try {
       const updateHansics = await prisma.hansics.update({
-        where: {
-          id: id,
+        where : {
+          id : id,
         },
-        data: {
-          lat: lat,
-          lng: lng,
+        data : {
+          lat : lat,
+          lng : lng,
         }
       });
 
       logger.info(updateHansics);
 
-      return { success: true };
+      return {success : true};
     } catch (err) {
       logger.error(err);
-      return { success: false };
+      return {success : false};
     }
   }
 
@@ -304,59 +295,50 @@ export class HansicService {
       const findHansic = await this.get(hansicId);
       console.log(findHansic);
 
-      if (!findHansic) return { success: false, status: 404 }
-      //transaction
-      prisma.$transaction(async (tx) => {
-        const selectFavorite = await tx.favorites.findFirst({
-          where: {
-            userId: body.userData.id,
-            hansicsId: hansicId,
+      if (!findHansic)
+        return {success : false, status : 404} // transaction
+        prisma.$transaction(async (tx) => {
+          const selectFavorite = await tx.favorites.findFirst({
+            where : {
+              userId : body.userData.id,
+              hansicsId : hansicId,
+            }
+          });
+
+          if (selectFavorite != null) {
+            // 즐겨찾기 데이터가 있을 시 update
+
+            const updateFavoite = selectFavorite.useFlag == true
+                                      ?
+
+                                      await tx.favorites.update({
+                                        where : {id : selectFavorite.id},
+                                        data : {useFlag : false}
+                                      })
+                                      :
+
+                                      await tx.favorites.update({
+                                        where : {id : selectFavorite.id},
+                                        data : {useFlag : true}
+                                      });
+          } else {
+            // 즐겨찾기 데이터가 없을 시 insert
+            const insertFavorite = await tx.favorites.create({
+              data : {
+                userId : body.userData.id,
+                hansicsId : hansicId,
+              }
+            });
           }
         });
 
-        if (selectFavorite != null) {
-          //즐겨찾기 데이터가 있을 시 update
-
-          const updateFavoite = selectFavorite.useFlag == true ?
-
-            await tx.favorites.update({
-              where: {
-                id: selectFavorite.id
-              },
-              data: {
-                useFlag: false
-              }
-            }) :
-
-            await tx.favorites.update({
-              where: {
-                id: selectFavorite.id
-              },
-              data: {
-                useFlag: true
-              }
-            });
-        } else {
-          //즐겨찾기 데이터가 없을 시 insert
-          const insertFavorite = await tx.favorites.create({
-            data: {
-              userId: body.userData.id,
-              hansicsId: hansicId,
-            }
-          });
-        }
-
-      });
-
-      return { success: true, status: 201 };
+      return {success : true, status : 201};
 
     } catch (err) {
       logger.error(err);
-      return { success: false, status: 500 };
+      return {success : false, status : 500};
     }
-
   }
-
 
   // async create(restaurantId:number,req:Request):Promise<boolean>
   // {
