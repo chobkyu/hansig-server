c6f8bc02cafd76a58ae1154b64b91fc445d689d1
chobkyu <98572357+chobkyu@users.noreply.github.com>
2024-03-15 10:57:54 +0900

    add testcase

--- src/service/admin.service.ts
+++ src/service/admin.service.ts
@@ -3,5 +3,12 @@ import { PrismaClient  } from "@prisma/client";
 const logger = require("../util/winston");
 
 export class AdminService{
-    
+    async getEnrollList(){
+        try{
+
+        }catch(err){
+            logger.error(err);
+            return {success : false};
+        }
+    }
 }
